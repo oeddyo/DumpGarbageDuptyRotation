b650ff9a264dcd9d62f4539405134877d4d414b2
Eddie Xie <exie@twitter.com>
2014-08-14 23:38:42 -0700

    Add

--- sendEmailNotification.py
+++ sendEmailNotification.py
@@ -1,19 +1,17 @@
 import smtplib
 
-
 listOfEmail = ['firstemail', 'secondemail', 'andsoon']
 import  random
 import config
-fromaddr = 'garbagenotification@gmail.com'
+fromaddr = 'YourEmailAddress'
 
 server = smtplib.SMTP('smtp.gmail.com:587')
 server.starttls()
 server.login(config.username, config.password)
 toaddrs = listOfEmail[random.randint(0, len(listOfEmail)-1)]
 
-toaddrs = "oeddyo@gmail.com"
-
 msg = "Dump garbage this week!"
+
 server.sendmail(fromaddr, toaddrs, msg)
 
 server.quit()
